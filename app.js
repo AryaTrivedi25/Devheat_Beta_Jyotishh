235651eee97f879e5cef9394a22fb2b2fb3b8c28
yashsojitra97 <yashsojitra97@gmail.com>
2023-10-01 01:39:14 +0530

    worked on integrating the api. working on bulk data fetching

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { log } = require("console");
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-    res.render("hi");
+app.get("/", function (req, res) {
+
+    let request = httpRequest();
+
+    request.then((array) => {
+        console.log(array);
+    })
+
+    res.render("index");
 })
 
-app.listen(3000, function() {
+async function httpRequest() {
+
+    let array = [];
+
+    for (let id = 1; id <= 20; id++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+        let promise = new Promise((resolve, reject) => {
+
+            https.get(url, res => {
+                let result = '';
+
+                res.on('data', data => {
+                    result += data;
+                });
+                res.on('end', () => {
+                    let pokemon = JSON.parse(result);
+                    resolve(pokemon);
+                });
+
+            });
+        })
+
+        let pokemonData = await promise;
+        console.log(pokemonData.forms[0].name);
+        array.push(pokemonData);
+    }
+    return array;
+}
+
+app.listen(3000, function () {
     console.log("server started at port 3000");
 })
